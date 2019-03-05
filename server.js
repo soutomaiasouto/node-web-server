7cf2bcee224feaf0a3a992a46a59e30abde4704c
maiasouto <soutomaiasouto@outlook.com>
2019-03-05 11:36:21 -0300

    initial commit

---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + "/views/partials")
+app.set('view engine', 'html');
+
+
+
+//.HBS -> .HTML
+app.engine('html', require('hbs').__express);
+
+
+
+app.use((req, res, next) => {
+
+var log = `${now}: ${req.method} ${req.url}`;
+var now = new Date().toString();
+
+    console.log(log)
+    fs.appendFile("server.log", log + "\n", (err) => {
+
+        if(err) {
+
+            console.log("Unable to append the file");
+        }
+
+        next();
+    });
+
+});
+
+// app.use((req, res, next) => {
+
+//     res.render("maintenance.hbs");
+
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getFullYear", () => {
+
+    return new Date().getFullYear();
+
+});
+
+hbs.registerHelper("uppercase", (text) => {
+
+    return text.toUpperCase();
+
+});
+
+
+app.get("/", (req, res) => {
+
+    res.render("index.hbs", {
+
+        pageTitle: "Apoio",
+        currentYear: new Date().getFullYear()
+
+    });
+
+});
+app.get("/about", (req, res) => {
+
+    res.render("about.html", {
+
+        pageTitle: "Sobre",
+        currentYear: new Date().getFullYear()
+
+    });
+    // res.send("<h2 style='text-align:center';>BAND ON THE RUN!</h2>")
+
+    });
+
+    app.get("/bad", (req, res) => {
+
+        res.send("404")
+        
+        
+    });
+
+
+app.listen(3000, () => {
+
+    console.log("Server is running on port 3000");
+
+});
